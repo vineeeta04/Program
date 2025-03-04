28765965b71954cc14bb618f478e3632923ea8f9
vineeeta04 <154489361+vineeeta04@users.noreply.github.com>
2025-03-04 14:59:43 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -3,10 +3,10 @@ class p1 {
     public static void main(String[] args) {
         Scanner n= new Scanner(System.in);
       int x,y,sum;
-    System.out.println("enter a number");
-    x=n.nextInt();
-     System.out.println("enter a number");
-          y=n.nextInt();
+    
+    x=2;
+    
+          y=5;
       sum=x+y;
            System.out.println("SUM IS: "+sum);
 
