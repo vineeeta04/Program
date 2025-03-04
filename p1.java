5c02dc0b4252d71be87eaea2776c83a81b1e785f
vineeeta04 <154489361+vineeeta04@users.noreply.github.com>
2025-03-04 14:52:16 +0530

    Create p1.java

---
+++ p1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class p1 {
+    public static void main(String[] args) {
+        Scanner n= new Scanner(System.in);
+      int x,y,sum;
+    System.out.println("enter a number");
+    x=n.nextInt();
+     System.out.println("enter a number");
+          y=n.nextInt();
+      sum=x+y;
+           System.out.println("SUM IS: "+sum);
+
+    }
+}
